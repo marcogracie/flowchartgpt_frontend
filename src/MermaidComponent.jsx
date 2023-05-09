d317077f0ff410e6e0a19d0c2a7229e1f8130d82
marcogracie <marco.gracie22@gmail.com>
2023-05-09 12:54:24 -0400

    now working on adding ability to zoom in and out on the chart

--- src/MermaidComponent.jsx
+++ src/MermaidComponent.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Mermaid from 'react-mermaid2';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const MermaidComponent = ({ chart }) => {
   return (
-    <div sx={{ width: '100%', height: '100%', overflow: 'auto'}}>
-      <Mermaid chart={chart} />
+    <div style={{ width: '100%', height: '100%', overflow: 'auto' }}>
+      <TransformWrapper
+        defaultScale={1}
+        defaultPositionX={200}
+        defaultPositionY={100}
+      >
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+            <div className="tools">
+              <button onClick={zoomIn}>+</button>
+              <button onClick={zoomOut}>-</button>
+              <button onClick={resetTransform}>x</button>
+            </div>
+            <TransformComponent>
+              <Mermaid chart={chart} />
+            </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
     </div>
   );
 };
 
 export default MermaidComponent;
+
