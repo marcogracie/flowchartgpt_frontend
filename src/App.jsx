b73e3b205dd4d0cbd3d94c6fb2f9421de29f9419
marcogracie <marco.gracie22@gmail.com>
2023-05-09 12:47:55 -0400

    mermaid component looks good, have to ensure it dynamically updates, it now scrolls on its own, going to try to add a way to zoom in and zoom out.

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,32 @@ import MermaidComponent from "./MermaidComponent";
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [messages, setMessages] = useState([]);
-  const [chart, setChart] = useState(`graph LR
-  A[Square Rect] -- Link text --> B((Circle))
-  A --> C(Round Rect)
-  B --> D{Rhombus}
-  C --> D`);
-
+  const [chart, setChart] = useState(`graph TD
+  Start --> A[Look for keys]
+  A -->|Found| B[Go to car]
+  A -->|Not found| C[Look in bag]
+  C --> D{In bag?}
+  D -->|Yes| B
+  D -->|No| E[Look in kitchen]
+  E --> F{In kitchen?}
+  F -->|Yes| B
+  F -->|No| G[Look in bedroom]
+  G --> H{In bedroom?}
+  H -->|Yes| B
+  H -->|No| I[Look in bathroom]
+  I --> J{In bathroom?}
+  J -->|Yes| B
+  J -->|No| K[Give up and take bus]
+  K --> End
+  B --> StartCar{Start car}
+  StartCar -->|Successful| Drive[Drive to work]
+  StartCar -->|Not successful| CallMechanic[Call mechanic]
+  Drive --> Work[Arrive at work]
+  Work -->|Day over| Home[Go home]
+  Home --> Relax[Relax and prepare for the next day]
+  Relax --> Sleep
+  Sleep --> Start
+`);
 
   const addMesssage = (newMessage) => {
     setMessages([...messages, { sender: "user", content: newMessage }]);
@@ -35,16 +55,23 @@ function App() {
           display: "flex",
           flexDirection: "column",
           minHeight: "100vh",
+          maxHeight: "100vh",  // Set a maximum height
           bgcolor: (theme) => theme.palette.background.default,
+          overflow: "hidden",  // Prevent overflow
         }}
       >
         {" "}
         <TopBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-        <Grid container sx={{ height: "calc(100vh - 56px)", p: 2 }}>
+        <Grid
+          container
+          sx={{
+            height: "calc(100vh - 56px)", p: 2, overflow: "hidden"
+          }}
+        >
           <Grid item xs={12} md={3}>
             <ChatBox messages={messages} onSendMessage={addMesssage} />
           </Grid>
-          <Grid item xs={12} md={9}>
+          <Grid item xs={12} md={9} sx={{ height: '100%', overflow: 'auto' }}>
             <MermaidComponent chart={chart} />
           </Grid>
         </Grid>
--- src/MermaidComponent.jsx
+++ src/MermaidComponent.jsx
@@ -3,7 +3,7 @@ import Mermaid from 'react-mermaid2';
 
 const MermaidComponent = ({ chart }) => {
   return (
-    <div>
+    <div sx={{ width: '100%', height: '100%', overflow: 'auto'}}>
       <Mermaid chart={chart} />
     </div>
   );
